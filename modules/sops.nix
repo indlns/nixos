73ddac03f8be705c12367e7a73eb6cf2b0f1b772
indlns <theindlns@gmail.com>
2026-06-02 00:08:02 +0500

    fix configs

--- modules/sops.nix
+++ modules/sops.nix
@@ -4,5 +4,5 @@
   sops.defaultSopsFile = "${inputs.self}/secrets/common.yaml";
   sops.age.keyFile = "/home/indlns/.config/sops/age/keys.txt";
   sops.defaultSopsFormat = "yaml";
-  sops.secrets.example-key = {};
+  sops.age.generateKey = false;
 }
