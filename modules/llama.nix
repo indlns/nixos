3ef2919934198cb4feb338c209d419443703cff4
indlns <theindlns@gmail.com>
2026-06-02 22:25:56 +0500

    add ollama.nix && update sops/llama.nix

--- modules/llama.nix
+++ modules/llama.nix
@@ -11,8 +11,12 @@ systemd.services.llama-cpp = {
     exec ${pkgs.llama-cpp-vulkan}/bin/llama-server \
       --host 0.0.0.0 \
       --port 8040 \
-      -m /var/lib/llama-cpp/models/gemma-4-E2B-it-Q4_K_M.gguf \
-      --ctx-size 4096
+      -m /var/lib/llama-cpp/models/Gemma-4-E2B-Uncensored-HauhauCS-Aggressive-Q2_K_P.gguf \
+      -ngl -1 \
+      -t 8 \
+      --threads-batch 10 \
+      -c 4096 \
+      -fa auto
   '';
 
   wantedBy = [ "multi-user.target" ];
--- modules/sops.nix
+++ modules/sops.nix
@@ -4,5 +4,6 @@
   sops.defaultSopsFile = "${inputs.self}/secrets/common.yaml";
   sops.age.keyFile = "/home/indlns/.config/sops/age/keys.txt";
   sops.defaultSopsFormat = "yaml";
+  sops.age.sshKeyPaths = [];
   sops.age.generateKey = false;
 }
