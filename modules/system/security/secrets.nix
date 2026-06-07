ec15c69bbfee3ce5d9f9f5fbdfe8afc005a510ea
indlns <theindlns@gmail.com>
2026-06-07 21:44:04 +0500

    update secret tokens

--- modules/system/security/secrets.nix
+++ modules/system/security/secrets.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  sops.secrets.cloudflare-token = {
-    format = "yaml";
-    sopsFile = ../../../secrets/common.yaml;
-    key = "cloudflare.token";
-  };
-
   sops.secrets.llama-api-key = {
     sopsFile = ../../../secrets/common.yaml;
     key = "api_tokens.llama";
