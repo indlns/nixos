dad6ebedcb5586b9f19b5f87dd5ce7fa602a368e
indlns <theindlns@gmail.com>
2026-06-16 14:43:48 +0500

    update docker config

--- modules/system/virtualization/docker.nix
+++ modules/system/virtualization/docker.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings = {
-    "default-address-pools" = [
-      { base = "172.80.0.0/16"; size = 24; }
-      ];
-    "log-level" = "info";
-    };
+  virtualisation.docker = {
+    enable = true;
+    # enableNvidia = true;
+    daemon.settings = {
+      "default-address-pools" = [
+        { base = "172.80.0.0/16"; size = 24; }
+        ];
+      "log-level" = "info";
+      };
+  };
 }
