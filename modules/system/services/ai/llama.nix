876761c70d967e3a0136edc72d000a0a7d5c4157
indlns <theindlns@gmail.com>
2026-06-24 20:54:18 +0500

    Commit all changes with descriptive message

--- modules/system/services/ai/llama.nix
+++ modules/system/services/ai/llama.nix
@@ -12,11 +12,11 @@
       --api-key "$(cat ${config.sops.secrets.llama-api-key.path})" \
       --host 0.0.0.0 \
       --port 8040 \
-      -m /var/lib/llama-cpp/models/Gemma-4-E2B-Uncensored-HauhauCS-Aggressive-Q2_K_P.gguf \
+      -m /var/lib/llama-cpp/models/qwen2.5-coder-14b-instruct.gguf \
       -ngl -1 \
       -t 16 \
       --threads-batch 16 \
-      -c 16384 \
+      -c 65536 \
       --flash-attn auto \
       --cont-batching \
   '';
